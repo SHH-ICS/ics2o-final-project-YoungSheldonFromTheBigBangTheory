10824595b254cc6c7178af1598022f85e60b36f3
YoungSheldonFromTheBigBangTheory <124380188+YoungSheldonFromTheBigBangTheory@users.noreply.github.com>
2023-06-14 14:50:30 +0000

    End of class

--- mainWorking.py
+++ mainWorking.py
@@ -34,7 +34,7 @@ def qBasic(): #Basic Question(Movement)
             return pos
         print("Invalid")
 
-def Interact():
+def qInteract():
     while True:
         for item in Inventory.inventory:
                 item:Item
@@ -49,29 +49,29 @@ def Interact():
 
 Inventory.inventory.append(Items.Spoon)
 
-def qInteract():
+def Interact():
     while True:
         temp = input("\n⇥   ")
         for item in Inventory.inventory:
             item:Item
             if item.Check(tempItem1) is True or tempItem1 == Items.Null:
-                if temp == "1" and tempPos1 != 1989:
+                if temp == "1" and tempPos1 != "null":
                     pos = tempPos1
                     return pos
 
-            if item.Check(tempItem2) is True or item.Check(Items.Null) is True:
-                if temp == "2" and tempPos2 != 1989:
+            if item.Check(tempItem2) is True or tempItem2 == Items.Null:
+                if temp == "2" and tempPos2 != "null":
                     pos = tempPos2
                     return pos
 
-            if item.Check(tempItem3) is True or item.Check(Items.Null) is True:
-                if temp == "3" and tempPos3 != 1989:
+            if item.Check(tempItem3) is True or tempItem3 == Items.Null:
+                if temp == "3" and tempPos3 != "null":
                     pos = tempPos3
                     return pos
             
-            if item.Check(tempItem4) is True or item.Check(Items.Null) is True:
+            if item.Check(tempItem4) is True or tempItem4 == Items.Null:
                 #Inventory.inventory.append(tempItem4)
-                if temp == "4" and tempPos4 != 1989:
+                if temp == "4" and tempPos4 != "null":
                     pos = tempPos4
                     return pos
 
@@ -93,12 +93,9 @@ while True: #Pos 0
         tempPos1 = 1 #Walk inside
         tempPos2 = 0.1 #Continue path left
         tempPos3 = 0.2 #Continue path right
-        tempPos4 = 1989 #Does nothing
-        tempItem1 = Items.Null #spoon
-        tempItem2 = Items.Null #beans
-        tempItem3 = Items.Null #Does nothing
-        tempItem4 = Items.Null
-        pos = qInteract()
+        tempPos4 = "null" #Does nothing
+        tempItem1 = tempItem2 = tempItem3 = tempItem4 = Items.Null 
+        pos = Interact()
     if pos == 1: #House Entrance
         Type("You find yourself in the entrance of the house")
         Type("\n1)Head upstairs\n2)Head straight towards kitchen\n3)Turn right to the living room\n4)Exit the house")
@@ -106,7 +103,8 @@ while True: #Pos 0
         tempPos2 = 1.11 #Head towards kitchen
         tempPos3 = 1.12 #Turn right to living room
         tempPos4 = 0 #Exit house
-        pos = qBasic()
+        tempItem1 = tempItem2 = tempItem3 = tempItem4 = Items.Null 
+        pos = Interact()
     if True: #Kitchen
         if pos == 1.11: #Kitchen
             Type("You step onto the black & white checkered floor. Close beside you to your left is a line of smooth white cabinets. ")
@@ -115,15 +113,17 @@ while True: #Pos 0
             tempPos2 = 1.112 #Seach drawer
             tempPos3 = 1.12 #Turn right to living room
             tempPos4 = 1 #Return to entrance
-            pos = qBasic()
+            tempItem1 = tempItem2 = tempItem3 = tempItem4 = Items.Null 
+            pos = Interact()
         if pos == 1.111: #Kitchen cabinet
             Type("You open the cabinet. There is nothing.")
             Type("\n1)Return")
             tempPos1 = 1.11 #Return to kitchen
-            tempPos2 = 1989 #Does nothing
-            tempPos3 = 1989 #Does nothing
-            tempPos4 = 1989 #Does nothing
-            pos = qBasic()
+            tempPos2 = "null" #Does nothing
+            tempPos3 = "null" #Does nothing
+            tempPos4 = "null" #Does nothing
+            tempItem1 = tempItem2 = tempItem3 = tempItem4 = Items.Null 
+            pos = Interact()
         if pos == 1.112: #Kitchen drawer  DOES NOT WORK
             Type("You pull open the drawer and peer inside.")
             #Type("\n1)Take", colored('spoon', 'red'), "\n2)Take", colored('canned beans', 'red'), "\n3)Return")
@@ -132,8 +132,26 @@ while True: #Pos 0
             tempItem2 = Items.Beans #beans
             tempItem3 = "null" #Does nothing
             tempItem4 = "null"
-            tempPos1 = 1989 #Does nothing
-            pos == qInteract()
+            tempPos1 = "null" #Does nothing
+            pos == Interact()
             
 
-    #f True: #Living Room
+    if True: #Living Room
+        if pos == 1.12: #Living Room
+            Type("The hard wood creaks below you as you enter the living room.")
+            Type("\n1)Look outside\n2)Go to kitchen\n3)Return to entrance")
+            tempPos1 = 1.121 #Look outside
+            tempPos2 = 1.11 #Go to kitchen
+            tempPos3 = 1.12 #Return to entrance
+            tempPos4 = "null" #Does nothing
+            tempItem1 = tempItem2 = tempItem3 = tempItem4 = Items.Null 
+            pos = Interact()
+        if pos == 1.121: #Living Room Window
+            Type("You aproach the window and look outside. It is dark. You see the faint outlines of trees")
+            Type("\n1)Return")
+            tempPos1 = 1.12 #Look outside
+            tempPos2 = "null" #Does nothing
+            tempPos3 = "null" #Does nothing
+            tempPos4 = "null" #Does nothing
+            tempItem1 = tempItem2 = tempItem3 = tempItem4 = Items.Null 
+            pos = Interact()
