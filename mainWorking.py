96835238df2d466fc1ee5d73033faa2830ee7aff
YoungSheldonFromTheBigBangTheory <124380188+YoungSheldonFromTheBigBangTheory@users.noreply.github.com>
2023-06-18 21:24:47 +0000

    heaven draws ever closer

--- mainWorking.py
+++ mainWorking.py
@@ -3,13 +3,10 @@ import sys, random, time
 from termcolor import colored, cprint
 from Inventory import *
 
-class Scene:
-    Room:str = "kitchen"
-    Target:str = None
-    Room:str = "spawn"
-
-
 pos = float(0)
+tempOpt:list = []
+tempPos:list = []
+tempItem:list = []
 
 wpm = 600 #wpm 85
 def Type(t): #Word typing control
@@ -19,8 +16,13 @@ def Type(t): #Word typing control
         time.sleep(random.random()*10.0/wpm)
 
 def Interact(): #Balls
+
+    for i in range(0, len(tempOpt)):
+        print((i + 1), ")", tempOpt[i])
+
     while True:
-        temp = input("\n⇥   ")
+
+        temp = input("⇥   ")
 
         if temp == "give items":
             #Inventory.inventory.append(Items.HouseKey)
@@ -89,55 +91,58 @@ while True: #Start Game
 while True: #Pos 0
 
     if pos == 0: #Spawn
-        Type("You find yourself on a rocky path in front of a slightly weathered, red bricked house")
-        Type("\n1)Walk inside\n2)Continue path left\n3)Continue path right")
-        tempPos:list = [1, 0.1, 0.2, "null"]
-        tempItem:list = [Items.HouseKey, Items.Null, Items.Null, Items.Null]
+        Type("You find yourself on a rocky path in front of a slightly weathered, red bricked house\n")
+        tempOpt = ["Walk inside", "Continue path left", "Continue path right"]
+        tempPos = [1, 0.1, 0.2, "null"]
+        tempItem = [Items.HouseKey, Items.Null, Items.Null, Items.Null]
         pos = Interact()
-    if pos == 1: #House Entrance
-        Type("You find yourself in the entrance of the house")
-        Type("\n1)Head upstairs\n2)Head straight towards kitchen\n3)Turn right to the living room\n4)Exit the house")
-        tempPos:list = [1.2, 1.11, 1.12, 0]
-        tempItem:list = [Items.Null, Items.Null, Items.Null, Items.Null] 
-        pos = Interact()
-    if True: #Kitchen
-        if pos == 1.11: #Kitchen
-            Type("You step onto the black & white checkered floor. Close beside you to your left is a line of smooth white cabinets. ")
-            Type("\n1)Search cabinet\n2)Search drawer\n3)Turn right to the living room\n4)Return to entrance")
-            tempPos:list = [1.111, 1.112, 1.12, 1]
-            tempItem:list = [Items.Null, Items.Null, Items.Null, Items.Null] 
-            pos = Interact()
-        if pos == 1.111: #Kitchen cabinet
-            Type("You open the cabinet. There is nothing.")
-            Type("\n1)Return")
-            tempPos:list = [1.11, "null", "null", "null"]
-            tempItem:list = [Items.Null, Items.Null, Items.Null, Items.Null] 
-            pos = Interact()
-        if pos == 1.112: #Kitchen drawer  DOES NOT WORK
-            Type("You pull open the drawer and peer inside.")
-            #Type("\n1)Take", colored('spoon', 'red'), "\n2)Take", colored('canned beans', 'red'), "\n3)Return")
-            temp = input("\n⇥   ")
-            tempItem1 = Items.Spoon #spoon
-            tempItem2 = Items.Beans #beans
-            tempItem3 = "null" #Does nothing
-            tempItem4 = "null"
-            tempPos1 = "null" #Does nothing
-            tempPos2 = "null" #Does nothing
-            tempPos3 = "null" #Does nothing
-            tempPos4 = "null" #Does nothing
-            pos = Interact()
-            
 
-    if True: #Living Room
-        if pos == 1.12: #Living Room
-            Type("The hard wood creaks below you as you enter the living room.")
-            Type("\n1)Look outside\n2)Go to kitchen\n3)Return to entrance")
-            tempPos:list = [1.121, 1.11, 1.12, "null"]
-            tempItem:list = [Items.Null, Items.Null, Items.Null, Items.Null] 
+    if True: #Inside house
+
+        if pos == 1: #House Entrance
+            Type("You find yourself in the entrance of the house\n")
+            tempOpt = ["Head upstairs", "Head straight towards kitchen", "Turn right to the living room", "Exit the house"]
+            tempPos = [1.2, 1.11, 1.12, 0]
+            tempItem = [Items.Null, Items.Null, Items.Null, Items.Null] 
             pos = Interact()
-        if pos == 1.121: #Living Room Window
-            Type("You aproach the window and look outside. It is dark. You see the faint outlines of trees")
-            Type("\n1)Return")
-            tempPos:list = [1.12, "null", "null", "null"]
-            tempItem:list = [Items.Null, Items.Null, Items.Null, Items.Null]
-            pos = Interact()
+        if True: #Kitchen
+            if pos == 1.11: #Kitchen
+                Type("You step onto the black & white checkered floor. Close beside you to your left is a line of smooth white cabinets. \n")
+                tempOpt = ["Search cabinet", "Search drawer", "Turn right to the living room", "Return to entrance"]
+                tempPos = [1.111, 1.112, 1.12, 1]
+                tempItem = [Items.Null, Items.Null, Items.Null, Items.Null] 
+                pos = Interact()
+            if pos == 1.111: #Kitchen cabinet
+                Type("You open the cabinet. There is nothing.")
+                tempOpt = ["Return"]
+                tempPos = [1.11, "null", "null", "null"]
+                tempItem = [Items.Null, Items.Null, Items.Null, Items.Null] 
+                pos = Interact()
+            if pos == 1.112: #Kitchen drawer  DOES NOT WORK
+                Type("You pull open the drawer and peer inside. \n")
+                #Type("\n1)Take", colored('spoon', 'red'), "\n2)Take", colored('canned beans', 'red'), "\n3)Return")
+                temp = input("\n⇥   ")
+                tempItem1 = Items.Spoon #spoon
+                tempItem2 = Items.Beans #beans
+                tempItem3 = "null" #Does nothing
+                tempItem4 = "null"
+                tempPos1 = "null" #Does nothing
+                tempPos2 = "null" #Does nothing
+                tempPos3 = "null" #Does nothing
+                tempPos4 = "null" #Does nothing
+                pos = Interact()
+                
+
+        if True: #Living Room
+            if pos == 1.12: #Living Room
+                Type("The hard wood creaks below you as you enter the living room. \n")
+                tempOpt = ["Look outside", "Go to kitchen", "Return to entrance"]
+                tempPos = [1.121, 1.11, 1.12, "null"]
+                tempItem = [Items.Null, Items.Null, Items.Null, Items.Null] 
+                pos = Interact()
+            if pos == 1.121: #Living Room Window
+                Type("You aproach the window and look outside. It is dark. You see the faint outlines of trees\n")
+                tempOpt = ["Return"]
+                tempPos = [1.12, "null", "null", "null"]
+                tempItem = [Items.Null, Items.Null, Items.Null, Items.Null]
+                pos = Interact()
